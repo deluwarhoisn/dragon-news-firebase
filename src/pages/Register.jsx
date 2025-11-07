edd47a3526edaae4938242da0026f5a0daf90289
deluwarhoisn <babuhossen301@gmail.com>
2025-11-07 22:30:53 +0600

    feat: integrate Firebase authentication and create AuthProvider
    - Added Firebase SDK to the project and configured Firebase in Firebase.config.js.
    - Created AuthProvider to manage authentication state and user creation.
    - Updated Navbar component to display user email and toggle login/logout button.
    - Enhanced Register component to handle user registration with Firebase.
    - Wrapped the application in AuthProvider for context access.

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,31 +1,57 @@
-import React from 'react';
+import React, { use } from 'react';
 import { Link } from 'react-router';
+import { AuthContext } from '../Provider/AuthProvider';
 
 const Register = () => {
+  const {createUser, setUser}=use(AuthContext);
+  const handleRegister= (e)=>{
+  e.preventDefault();
+  console.log(e.target);
+  const form = e.target;
+  const name= form.name.value;
+  const photo= form.photo.value;
+  const email= form.email.value;
+  const password= form.password.value;
+  console.log({name,photo,email,password});
+  createUser(email,password)
+    .then(result=>{
+      const user= result.user
+      // console.log(user)
+      setUser(user);
+    })
+   .catch((error) => {
+    // const errorCode = error.code;
+    const errorMessage = error.message;
+  alert(errorMessage)
+  });
+    
+  }
     return (
             <div className='flex justify-center min-h-screen items-center'>
          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5">
             <h2 className='font-semibold text-2xl text-center'>Register your account</h2>
-      <div className="card-body">
+      <form onSubmit={handleRegister} className="card-body">
         <fieldset className="fieldset">
-             <label className="label font-bold">Your Name</label>
+          {/* name */}
+             <label className="label font-bold" name="name"> Your Name</label>
           <input type="email" className="input" placeholder="Enter your name" />
-           <label className="label font-bold">Photo URL</label>
+          {/* photo */}
+           <label className="label font-bold" name="photo">Photo URL</label>
           <input type="email" className="input" placeholder="Enter your password" />
             {/* email */}
-          <label className="label font-bold">Email</label>
-          <input type="email" className="input" placeholder="Email" />
+          <label className="label font-bold" name="email">Email</label>
+          <input type="email" className="input" placeholder="Email" required/>
 
           {/* password */}
-          <label className="label font-bold">Password</label>
-          <input type="password" className="input" placeholder="Password" />
+          <label className="label font-bold" name="password">Password</label>
+          <input type="password" className="input" placeholder="Password" required/>
 
        
           
-          <button className="btn btn-neutral mt-4">Register</button>
+          <button type="submit" className="btn btn-neutral mt-4">Register</button>
           <p className='text-center font-semibold pt-5'>Allready Have An Account ?<Link className='text-secondary' to="/auth/login">Login</Link></p>
         </fieldset>
-      </div>
+      </form>
     </div>
         </div>
     );
